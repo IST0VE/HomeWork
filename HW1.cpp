2c489fd21c2564810b3dc2205323b27a5389340c
IST0VE <74756686+IST0VE@users.noreply.github.com>
2021-03-01 10:00:26 +0400

    Add files via upload

---
+++ HW1.cpp
@@ -0,0 +1,13 @@
+﻿#include <iostream>
+
+int main()
+{
+	int a = 7;
+	int b = 45;
+	int c = 10;
+	int d = 34;
+
+	double x = (double)a * ((double)b + ((double)c / (double)d));
+
+	std::cout << x;
+}
